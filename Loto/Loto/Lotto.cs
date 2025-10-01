32eecf9bc0aaccab718cd53f04fb320a1afec865
ARGUMENT <vitalik.papkov@mail.ru>
2025-10-01 20:31:46 +0300

    Переименование полей

---
+++ Loto/Loto/Lotto.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lotto
+{
+    class Lotto
+    {
+        private readonly List<int> _lossHistory = new List<int>();
+        private static Random rnd = new Random();
+        public readonly int _numberOfGames = 99;
+        private static Dictionary<int, int> numberStatistics = new Dictionary<int, int>();
+
+        public int GetNumber()
+        {
+            if (_lossHistory.Count >= _numberOfGames)
+            {
+                Console.Write("\nВ диапазоне нехватает чисел: ");
+                return -1;
+            }
+
+            int returnNumber;
+
+            do
+            {
+                returnNumber = rnd.Next(1, 100);
+            }
+            while (_lossHistory.Contains(returnNumber));
+            _lossHistory.Add(returnNumber);
+
+            if (numberStatistics.ContainsKey(returnNumber))
+                numberStatistics[returnNumber]++;
+            else
+                numberStatistics[returnNumber] = 1;
+            return returnNumber;
+
+
+        }
+
+        public static void PrintGamesStats()
+        {
+            if (numberStatistics.Count == 0)
+            {
+                Console.WriteLine("Числа еще не выпадали");
+                return;
+            }   
+
+            List<int> sortedKeys = new List<int>(numberStatistics.Keys);
+            sortedKeys.Sort();
+
+            Console.WriteLine();
+            foreach (int key in sortedKeys)
+            {
+                Console.WriteLine($"Число {key}: выпало {numberStatistics[key]} раз");
+            }
+        }
+    }
+}
--- Loto/Loto/Program.cs
+++ Loto/Loto/Program.cs
@@ -14,7 +14,6 @@ namespace Lotto
     {
         static void Main(string[] args)
         {
-            Console.WriteLine();
             Console.WriteLine("\n\nИгра 1");
             Lotto loto1 = new Lotto();
             for (int i = 0; i < 50; i++)
@@ -66,58 +65,6 @@ namespace Lotto
             }
 
             Lotto.PrintGamesStats();
-        }
-    }
-
-    class Lotto
-    {
-        private List<int> lossHistory = new List<int>();
-        private static Random rnd = new Random();
-        private int numberOfGames = 99;
-        private static Dictionary<int, int> numberStatistics = new Dictionary<int, int>();
-
-        public int GetNumber()
-        {
-            if (lossHistory.Count >= numberOfGames)
-            {
-                Console.Write("\nВ диапазоне нехватает чисел: ");
-                return -1;
-            }
-
-            int returnNumber;
-
-            do
-            {
-                returnNumber = rnd.Next(1, 100);
-            }
-            while (lossHistory.Contains(returnNumber)) ;
-            lossHistory.Add(returnNumber);
-
-            if (numberStatistics.ContainsKey(returnNumber))
-                numberStatistics[returnNumber]++;
-            else
-                numberStatistics[returnNumber] = 1; 
-            return returnNumber;
-
-
-        }
-
-        public static void PrintGamesStats()
-        {
-            if (numberStatistics.Count == 0)
-            {
-                Console.WriteLine("Числа еще не выпадали");
-                return;
-            }
-
-            List<int> sortedKeys = new List<int>(numberStatistics.Keys);
-            sortedKeys.Sort();
-
-            Console.WriteLine();
-            foreach (int key in sortedKeys)
-            {
-                Console.WriteLine($"Число {key}: выпало {numberStatistics[key]} раз");
-            }
         }
     }
 }
